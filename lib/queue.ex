da2161f372f62a0e4dce5803ec8c46e842609032
adam-james <adam.j.sulewski@gmail.com>
2020-05-19 00:41:24 -0400

    add some functions

--- lib/queue.ex
+++ lib/queue.ex
@@ -1,18 +1,102 @@
 defmodule Queue do
   @moduledoc """
-  Documentation for `Queue`.
+  A wrapper around Erlang's `:queue` module.
   """
 
+  # TODO add type specs
+  # TODO improve docs
+
+  defstruct store: :queue.new()
+
   @doc """
-  Hello world.
+  Returns an empty queue.
 
   ## Examples
 
-      iex> Queue.hello()
-      :world
+      iex> Queue.new()
+      #Queue<[]>
 
   """
-  def hello do
-    :world
+  def new do
+    :queue.new |> wrap_store
+  end
+
+  @doc """
+  Returns a queue from a list.
+
+  ## Examples
+
+      iex> Queue.from_list([1,2,3])
+      #Queue<[1, 2, 3]>
+
+  """
+  def from_list(list) do
+    list |> :queue.from_list |> wrap_store
+  end
+
+  @doc """
+  Filters a queue.
+
+  ## Examples
+
+      iex> queue = Queue.from_list([1,2,3,4])
+      iex> Queue.filter(queue, fn item -> rem(item, 2) != 0 end)
+      #Queue<[1, 3]>
+
+  """
+  def filter(queue, func) do
+    queue |> unwrap_store |> do_filter(func) |> wrap_store
+  end
+
+  defp do_filter(store, func) do
+    :queue.filter(func, store)
+  end
+
+  @doc """
+  Returns a list of items in a queue.
+
+  ## Examples
+
+      iex> queue = Queue.from_list([1, 2, 3, 4])
+      iex> Queue.to_list(queue)
+      [1, 2, 3, 4]
+
+  """
+  def to_list(queue) do
+    queue |> unwrap_store |> :queue.to_list
+  end
+
+  @doc """
+  Removes item from the front of the queue.
+
+  # TODO fix this example. When using #Queue it must be at the beginning of expression.
+  # TODO maybe change the inspect output?
+
+  # ## Examples
+
+  #     iex> queue = Queue.from_list([1, 2])
+  #     iex> Queue.out(queue)
+  #     {{:value, 1}, #Queue<[2]>}
+
+  """
+  def out(queue) do
+    store = queue |> unwrap_store
+
+    case :queue.out(store) do
+      {{:value, item}, updated_store} ->
+        {{:value, item}, wrap_store(updated_store)}
+      {:empty, updated_store} ->
+        {:empty, wrap_store(updated_store)}
+    end
+  end
+
+  defp unwrap_store(%Queue{store: store}), do: store
+
+  defp wrap_store(store), do: %Queue{store: store}
+
+  defimpl Inspect do
+    def inspect(queue, _) do
+      "#Queue<#{inspect(:queue.to_list(queue.store))}>"
+    end
   end
 end
--- test/queue_test.exs
+++ test/queue_test.exs
@@ -1,8 +1,44 @@
 defmodule QueueTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Queue
 
-  test "greets the world" do
-    assert Queue.hello() == :world
+  describe "new/0" do
+    test "returns an empty queue" do
+      assert Queue.new() == %Queue{store: :queue.new()}
+    end
+  end
+
+  describe "out/1" do
+    test "removes item from the queue" do
+      queue = Queue.from_list [1, 2]
+      assert {{:value, 1}, %Queue{} = queue} = Queue.out(queue)
+      assert {{:value, 2}, %Queue{} = queue} = Queue.out(queue)
+      assert {:empty, queue} = Queue.out(queue)
+    end
+  end
+
+  describe "from_list/1" do
+    test "returns a queue from a list" do
+      list = [1,2,3]
+      queue = Queue.from_list(list)
+      assert Queue.to_list(queue) == list
+    end
+  end
+
+  describe "to_list/1" do
+    test "returns a list of items in the queue" do
+      list = [1, 2, 3, 4]
+      queue = Queue.from_list list
+      assert Queue.to_list(queue) == list
+    end
+  end
+
+  describe "filter/2" do
+    test "filters items in a queue" do
+      queue = Queue.from_list [1, 2, 3, 4]
+      even_only = fn item -> rem(item, 2) == 0 end
+      queue = Queue.filter(queue, even_only)
+      assert queue == Queue.from_list [2, 4]
+    end
   end
 end
