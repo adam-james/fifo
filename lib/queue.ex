c13ffcf6ab9cc3c97539b5cc287978c807c508b8
adam-james <adam.j.sulewski@gmail.com>
2020-05-19 21:59:30 -0400

    add more functions

--- lib/queue.ex
+++ lib/queue.ex
@@ -30,7 +30,7 @@ defmodule Queue do
       #Queue<[1, 2, 3]>
 
   """
-  def from_list(list) do
+  def from_list(list) when is_list(list) do
     list |> :queue.from_list |> wrap_store
   end
 
@@ -44,8 +44,8 @@ defmodule Queue do
       #Queue<[1, 3]>
 
   """
-  def filter(queue, func) do
-    queue |> unwrap_store |> do_filter(func) |> wrap_store
+  def filter(%Queue{store: store}, func) do
+    store |> do_filter(func) |> wrap_store
   end
 
   defp do_filter(store, func) do
@@ -62,9 +62,7 @@ defmodule Queue do
       [1, 2, 3, 4]
 
   """
-  def to_list(queue) do
-    queue |> unwrap_store |> :queue.to_list
-  end
+  def to_list(%Queue{store: store}), do: :queue.to_list(store)
 
   @doc """
   Removes item from the front of the queue.
@@ -82,9 +80,7 @@ defmodule Queue do
       #Queue<[]>
 
   """
-  def out(queue) do
-    store = queue |> unwrap_store
-
+  def out(%Queue{store: store}) do
     case :queue.out(store) do
       {{:value, item}, updated_store} ->
         {{:value, item}, wrap_store(updated_store)}
@@ -93,9 +89,89 @@ defmodule Queue do
     end
   end
 
-  # TODO continue to add functions from the docs: https://erlang.org/doc/man/queue.html
+  @doc """
+  Enqueues an item at the end of the queue. The Erlang library uses the name
+  `in` for this function. This is not allowed in Elixir as `in` is a reserved word.
+  For that reason, I have called this `in_l` for now. I am considering alternatives.
 
-  defp unwrap_store(%Queue{store: store}), do: store
+  ## Examples
+
+      iex> queue = Queue.from_list([1, 2])
+      iex> Queue.in_l(queue, 3)
+      #Queue<[1, 2, 3]>
+
+  """
+  def in_l(%Queue{store: store}, item) do
+    :queue.in(item, store) |> wrap_store
+  end
+
+  @doc """
+  Enqueues an item at the front of the queue.
+
+  ## Examples
+
+      iex> queue = Queue.from_list([1, 2])
+      iex> Queue.in_r(queue, 3)
+      #Queue<[3, 1, 2]>
+
+  """
+  def in_r(%Queue{store: store}, item) do
+    :queue.in_r(item, store) |> wrap_store
+  end
+
+  @doc """
+  Returns `true` if the queue has not items. Returns `false` if the queue has items.
+
+  ## Examples
+
+      iex> queue = Queue.new
+      iex> Queue.empty?(queue)
+      true
+
+      iex> queue = Queue.from_list([1])
+      iex> Queue.empty?(queue)
+      false
+
+  """
+  def empty?(%Queue{store: store}), do: :queue.is_empty(store)
+
+  @doc """
+  Returns `true` given value is a queue. Returns `false` if not. It also check the
+  underlying implementation, ensuring the underlying implementation is also a queue.
+
+  ## Examples
+
+      iex> Queue.queue? Queue.new
+      true
+
+      iex> Queue.queue? []
+      false
+
+      iex> Queue.queue? %Queue{store: []}
+      false
+
+  """
+  def queue?(%Queue{store: store}), do: :queue.is_queue(store)
+
+  def queue?(_), do: false
+
+  @doc """
+  Returns a new queue which is a combination of `queue1` and `queue2`. `queue1`
+  is in front for `queue2`.
+
+  ## Examples
+
+      iex> queue1 = Queue.from_list([1, 2])
+      iex> queue2 = Queue.from_list([3, 4])
+      iex> Queue.join(queue1, queue2)
+      #Queue<[1, 2, 3, 4]>
+
+  """
+  def join(%Queue{store: store1}, %Queue{store: store2}) do
+    :queue.join(store1, store2) |> wrap_store
+  end
+
+  # TODO continue to add functions from the docs: https://erlang.org/doc/man/queue.html
 
   defp wrap_store(store), do: %Queue{store: store}
 
--- test/queue_test.exs
+++ test/queue_test.exs
@@ -8,15 +8,6 @@ defmodule QueueTest do
     end
   end
 
-  describe "out/1" do
-    test "removes item from the queue" do
-      queue = Queue.from_list [1, 2]
-      assert {{:value, 1}, %Queue{} = queue} = Queue.out(queue)
-      assert {{:value, 2}, %Queue{} = queue} = Queue.out(queue)
-      assert {:empty, queue} = Queue.out(queue)
-    end
-  end
-
   describe "from_list/1" do
     test "returns a queue from a list" do
       list = [1,2,3]
@@ -30,6 +21,70 @@ defmodule QueueTest do
       list = [1, 2, 3, 4]
       queue = Queue.from_list list
       assert Queue.to_list(queue) == list
+    end
+  end
+
+  describe "in_l/2" do
+    test "enqueues an item at end of queue" do
+      queue = Queue.from_list [1, 2]
+      queue = Queue.in_l(queue, 3)
+      assert Queue.to_list(queue) == [1, 2, 3]
+    end
+  end
+
+  describe "in_r/2" do
+    test "enqueues an item at the front of the queue" do
+      queue = Queue.from_list [1, 2]
+      queue = Queue.in_r(queue, 3)
+      assert Queue.to_list(queue) == [3, 1, 2]
+    end
+  end
+
+  describe "empty?/1" do
+    test "returns true when queue is empty" do
+      queue = Queue.new
+      assert Queue.empty?(queue) == true
+    end
+
+    test "returns false when queue is not empty" do
+      queue = Queue.from_list [1]
+      assert Queue.empty?(queue) == false
+    end
+  end
+
+  describe "queue?/1" do
+    test "returns true when is a queue" do
+      queue = Queue.new
+      assert Queue.queue?(queue) == true
+    end
+
+    test "returns false when is not queue" do
+      assert Queue.queue?(%{}) == false
+      assert Queue.queue?(4) == false
+      assert Queue.queue?("test") == false
+    end
+
+    test "returns false when underlying implementation is not a queue" do
+      queue = %Queue{store: []}
+      assert Queue.queue?(queue) == false
+    end
+  end
+
+  describe "join/2" do
+    test "joins two queues with the first given in front" do
+      queue1 = Queue.from_list([1, 2])
+      queue2 = Queue.from_list([3, 4])
+      queue3 = Queue.join(queue1, queue2)
+      assert Queue.to_list(queue3) == [1, 2, 3, 4]
+    end
+  end
+
+  describe "out/1" do
+    test "removes item from the queue" do
+      queue = Queue.from_list [1, 2]
+      assert {{:value, 1}, %Queue{} = queue} = Queue.out(queue)
+      assert {{:value, 2}, %Queue{} = queue} = Queue.out(queue)
+      assert {:empty, queue} = Queue.out(queue)
     end
   end
 
