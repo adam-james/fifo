ba3e36e2e815b2edcf4be79988c326a9f158e6f5
adam-james <adam.j.sulewski@gmail.com>
2020-05-27 09:57:33 -0400

    add front page to docs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,16 @@
 defmodule Queue.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :queue,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs()
     ]
   end
 
@@ -23,6 +26,17 @@ defmodule Queue.MixProject do
     [
       {:ex_doc, "~> 0.22", only: :dev, runtime: false},
       {:benchee, "~> 1.0", only: :dev}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        "README.md"
+      ],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/adam-james/queue"
     ]
   end
 end
