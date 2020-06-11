ea2229023d06e87272aa1e9719e771faf040faec
adam-james <adam.j.sulewski@gmail.com>
2020-06-11 08:41:04 -0400

    publish to hex

--- mix.exs
+++ mix.exs
@@ -2,6 +2,7 @@ defmodule FIFO.MixProject do
   use Mix.Project
 
   @version "0.1.0"
+  @description "A first-in-first-out queue data structure for Elixir."
 
   def project do
     [
@@ -10,7 +11,9 @@ defmodule FIFO.MixProject do
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      docs: docs()
+      docs: docs(),
+      description: @description,
+      package: package()
     ]
   end
 
@@ -19,6 +22,15 @@ defmodule FIFO.MixProject do
     [
       extra_applications: [:logger]
     ]
+  end
+
+  defp package do
+    %{
+      licenses: ["Apache-2.0"],
+      maintainers: ["Adam Sulewski"],
+      links: %{"GitHub" => "https://github.com/adam-james/fifo"},
+      files: ["lib", "mix.exs", "README.md", "bench", ".formatter.exs"]
+    }
   end
 
   # Run "mix help deps" to learn about dependencies.
